83abf2922a5c5eddcd4ba041bd97e833656b5798
Miulle <mika.strandb@gmail.com>
2019-06-30 17:51:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/booksurfing/AddBookActivity.java
@@ -0,0 +1,13 @@
+package com.example.booksurfing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listbook_layout);
+    }
+}
---
+++ app/src/main/java/com/example/booksurfing/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.booksurfing;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "ContentMainActivity";
+
+    DatabaseHelper databaseHelper;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        databaseHelper = new DatabaseHelper(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    int i = 0;
+
+    //TODO new View and fields - fill manually or per ISBN-Scanner
+    public void scanOnClick(View view) {
+
+//        TextView textScan = findViewById(R.id.textScanView);
+//        i++;
+//        textScan.setText("Scan Button Clicked..." + i);
+    }
+
+    //TODO ListView of Scanned Items
+    public void viewLibraryOnClick(View view) {
+        //change intent
+        //TODO List all Items in SQL DB
+        Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+        startActivity(intent);
+
+        //TODO click on single item to display information
+
+    }
+
+    public void btnChangeIntent(View view) {
+        Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+        startActivity(intent);
+    }
+
+    public void btnAddData(View view) {
+        TextView textView = findViewById(R.id.textScanView);
+        String newEntry = textView.getText().toString();
+//        toastMessage(databaseHelper.testfct());
+
+        //        toastMessage(newEntry);
+//        addData(newEntry);
+        if (newEntry.length() != 0) {
+            addData(newEntry);
+            textView.setText("");
+        } else {
+            toastMessage("You must put something into the text field");
+        }
+    }
+
+    public void addData(String s) {
+        toastMessage("STRING: " + s);
+        boolean insertData = databaseHelper.addData(s);
+//        if (insertData) {
+//            toastMessage("Data successfully added");
+//        } else {
+//            toastMessage("Something when wrong");
+//        }
+    }
+}
